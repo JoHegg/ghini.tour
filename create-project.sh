8306d42040964093c049ad880c54302e4a2cb98d
Mario Frasca <mario@anche.no>
2017-07-13 16:26:56 -0500

    cleaning up, related to #2

--- create-project.sh
+++ create-project.sh
@@ -2,23 +2,44 @@
 PREVDIR=$(pwd)
 BASEDIR=$(cd "$(dirname "$0")"; pwd)
 PROJECT=$1
+
+if [ ! -d $BASEDIR/project-data/$PROJECT ]
+then
+    echo --------------------------------------
+    echo Creating project data directory, initializing from audiowand
+    rsync -ah $BASEDIR/audiowand/data/* $BASEDIR/project-data/$PROJECT
+    echo Putting it under version control, you still need commit
+    git add $BASEDIR/project-data/$PROJECT
+fi
+
+echo --------------------------------------
 cd $BASEDIR/cordova-projects/
-cordova create $PROJECT uk.org.rbge.hyam.audiowand.$PROJECT $PROJECT
+cordova create $PROJECT uk.org.rbge.hyam.audiowand.$PROJECT $PROJECT || exit 1
 cd $PROJECT
+
+echo --------------------------------------
+echo Initializing www structure from template
 rsync -ah --exclude-from $BASEDIR/audiowand/exclude_from_build.txt $BASEDIR/audiowand/* www
-cp audiowand/config.xml www
+cp $BASEDIR/audiowand/config.xml www
 rm config.xml
 ln -s www/config.xml .
 rm -fr www/data
 ln -s $BASEDIR/project-data/$PROJECT www/data
-cordova plugin add cordova-plugin-background-audio@1.0.0
-cordova plugin add cordova-plugin-device@1.0.1
-cordova plugin add cordova-plugin-file@3.0.0
-cordova plugin add cordova-plugin-geolocation@1.0.1
-cordova plugin add cordova-plugin-inappbrowser@1.0.1
-cordova plugin add cordova-plugin-media@1.0.1
-cordova plugin add cordova-plugin-splashscreen@2.1.0
-cordova plugin add cordova-plugin-statusbar@1.0.1
-echo adding platform, will take time, please be patient.
+
+echo --------------------------------------
+echo Adding plugins ...
+cordova plugin add \
+        cordova-plugin-statusbar \
+        cordova-plugin-media \
+        cordova-plugin-splashscreen \
+        cordova-plugin-background-audio \
+        cordova-plugin-device \
+        cordova-plugin-file \
+        cordova-plugin-geolocation \
+        cordova-plugin-inappbrowser@0.2.2 \
+
+echo --------------------------------------
+echo Adding platform, might take time, please be patient.
 cordova platform add android
+
 cd $PREVDIR
